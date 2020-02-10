9da87a1706c12ded911eabb7f1c32eed017c5a31
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-10 21:48:23 +0000

    edited  index file

--- index.js
+++ index.js
@@ -2,9 +2,39 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  takeUntil: takeUntil,
+  without: without,
+  map: map,
+  letterPositions: letterPositions,
+  flatten: flatten,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  countOnly: countOnly,
+  countLetters: countLetters
 };
